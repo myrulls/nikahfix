6e419d74b81d50d7d58528cc2d855b91d2954192
Myrul <102006236+myrulls@users.noreply.github.com>
2024-11-23 16:43:40 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: "/deploy_react_app_github_pages_vercel"
+  base: "/deploy_react_app_github_pages_vercel"
+  const router = createBrowserRouter(routes, { basename: import.meta.env.BASE_URL });
 })
